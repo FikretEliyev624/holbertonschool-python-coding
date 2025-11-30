4406604925f97c5303691c83ffb6b00b84af97ab
Fikret Eliyev <fikret.eliyev.2001@gmail.com>
2025-11-30 16:46:30 +0400

    task-1

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
+"""
+0-square module
+
+"""
+
+
 class Square:
     """Defines a square"""
 
     def __init__(self, size):
         """Initialize a square with a private size attribute"""
         self.__size = size
-
